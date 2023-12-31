b743151bd5e1fd17f3a879f8439a4a983eb40ca3
Hamza <h4hamzashoaib@gmail.com>
2023-12-31 23:35:48 +0500

    First Assignment

---
+++ main.dart
@@ -0,0 +1,170 @@
+void main(){
+//   Question no 1
+//   int length = 12;
+//   int breath = 22;
+//   if(length==breath){
+//     print('square values');
+//   }else{
+//     print('rectangle values');
+//   }
+//   Question no 2
+//   int p1_age = 20;
+//   int p2_age = 10;
+//   if(p1_age>p2_age){
+//     print('Person one is Older and Person two is younger');
+//   }
+//   else if(p2_age>p1_age){
+//     print('Person two is Older and Person one is younger');
+//   }
+//   else{
+//     print('both are of same age');
+//   }
+//   Question no 3
+//   num heldClasses = 16;
+//   num attendClasses = 10;
+//   num per = (attendClasses/heldClasses)*100;
+//   print(per);
+//   if(per<75){
+//     print('With this Attendence $per%  you arent allow sit in exam');
+//   }else{
+//     print('With this Attendence $per%  you are allow sit in exam');
+//   }
+//   Question no 4
+//    var celcius = 19;
+//    var farenhite =( celcius * 9/5)+32;
+//    print('Temperature in Farenhite $farenhite');
+//   Question no 5
+//   num temp = 20;
+//   if(temp<=0){
+//     print('freezing whether');
+//   }else if(temp>0 && temp <=10){
+//     print('Very Cold whether');
+//    }
+//   else if(temp>10 && temp <=20){
+//     print(' Cold whether');
+//    }
+//   else if(temp>30 && temp <=40){
+//     print('Hot whether');
+//    }
+//   else{
+//     print('very Hot');
+//   }
+// Question no 6 
+//  var alphabet = 'e';
+//   if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+//     print('$alphabet is a vowel.');
+//   } else {
+//     print('$alphabet is a consonant.');
+//   }
+// }
+//   Question no 7
+//   num y = 12;
+//   print('${y*y} here is the square root of $y');
+// Question no 8
+//     String studentname = 'Hamza';
+//     num studentrollnum = 1290;
+//     String className = 'flutter';
+//     num percentage ;
+//     num total_marks = 500;
+  
+//     num english_marks =80;  
+//     num math_marks =50;  
+//     num chem_marks =60;  
+//     num phy_marks =70;  
+//     num bio_marks =65;
+  
+//   num obtmarks = english_marks+math_marks+chem_marks+phy_marks+bio_marks;
+//   percentage = (obtmarks/total_marks)*100;
+  
+  
+//   String grade;
+//   if(percentage<=40){
+//     grade = 'F';
+//   }
+//   else if(percentage>40 && percentage<=50){
+//     grade = 'E';
+//   }
+//   else if(percentage>50 && percentage<=60){
+//     grade = 'C';
+//   }
+//   else if(percentage>60 && percentage<=70){
+//     grade = 'B';
+//   }
+//   else if(percentage>70 && percentage<=80){
+//     grade = 'A';
+//   }
+//   else if(percentage>80 && percentage<=90){
+//     grade = 'A+';
+//   }
+//   else{
+//     grade = 'Topper';
+//   }
+  
+//   print("***************************************************");
+//   print("             MARKSHEET");
+//   print("***************************************************");
+//   print("Student Name: $studentname");
+//   print("Roll Number: $studentrollnum");
+//   print("Class: $className");
+//   print("---------------------------------------------------");
+//   print("Subject | Marks");
+//   print("---------------------------------------------------");
+//   print("Math    | $math_marks");
+//   print("Chemistry | $chem_marks");
+//   print("English | $english_marks");
+//   print("Physics | $phy_marks");
+//   print("Biology| $bio_marks");
+//   print("---------------------------------------------------");
+//   print("Total Marks: $total_marks");
+//   print("Obtained Marks: $obtmarks");
+//   print("Percentage: $percentage%");
+//   print("Grade: $grade");
+//   print("***************************************************");
+    
+  
+  
+// Question no 9
+//   int number = 25; // Replace with the number you want to check
+
+//   if (number % 2 == 0) {
+//     // Number is even
+//     if (number % 5 == 0) {
+//       print("The number $number is even and divisible by 5.");
+//     } else {
+//       print("The number $number is even but not divisible by 5.");
+//     }
+//   } else {
+//     // Number is odd
+//     if (number % 7 == 0) {
+//       print("The number $number is odd and divisible by 7.");
+//     } else {
+//       print("The number $number is odd but not divisible by 7.");
+//     }
+//   }
+// Question no 10
+// int num1 = int.parse(stdin.readLineSync());
+//  int num2 = int.parse(stdin.readLineSync());
+//  int num3 = int.parse(stdin.readLineSync());
+
+//  // Find the greatest number
+//  int greatest = num1;
+//  if (num2 > greatest) {
+//    greatest = num2;
+//  }
+//  if (num3 > greatest) {
+//    greatest = num3;
+//  }
+
+//  // Find the lowest number
+//  int lowest = num1;
+//  if (num2 < lowest) {
+//    lowest = num2;
+//  }
+//  if (num3 < lowest) {
+//    lowest = num3;
+//  }
+
+//  // Print the results
+//  print("The greatest number is: $greatest");
+//  print("The lowest number is: $lowest");
+
